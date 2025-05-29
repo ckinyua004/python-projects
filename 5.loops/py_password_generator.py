965f8394433f236a83b33c32ddc45f219c6f2e44
ckinyua004 <njauc85@gmail.com>
2025-05-29 18:42:15 +0300

    Test commit

--- 5.loops/py_password_generator.py
+++ 5.loops/py_password_generator.py
@@ -10,21 +10,21 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '_', '{', '}', '[', ']']
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ,'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#Soft version
+#Easy version
 
-password = ""
+easy_password = ""
 
 for char in range(1, nr_letters + 1):
     random_char = random.choice(letters)
-    password += random_char
+    easy_password += random_char
 
 for sym in range(1, nr_symbols + 1):
     random_sym = random.choice(symbols)
-    password += random_sym
+    easy_password += random_sym
 
 for num in range(1, nr_numbers + 1):
     random_num = random.choice(numbers)
-    password += random_num
+    easy_password += random_num
 
 #hard version
 password_list = []
@@ -47,5 +47,5 @@ for char in password_list:
     hard_password += char
 
 
-print(f"Your generated easy password is: {password}")
+print(f"Your generated easy password is: {easy_password}")
 print(f"Your generated hard password is {hard_password}")
