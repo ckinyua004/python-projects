6f5c81405af0720cae56297c03d90350763bf997
ckinyua004 <njauc85@gmail.com>
2025-06-19 20:41:53 +0300

    Created a snake game -Added move abilities to the snake

--- 21.SnakeGame/main.py
+++ 21.SnakeGame/main.py
@@ -1,4 +1,6 @@
 from turtle import Screen, Turtle
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -6,6 +8,19 @@ screen.bgcolor("green")
 screen.title("MY SNAKE GAME")
 screen.tracer(0)
 
-screen.update()
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+    
+
 screen.exitonclick()
-It Looks Like you are not t connected tothe internet -
