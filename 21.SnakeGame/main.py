bcb65b165bd8dfe2d278a9c56feee1efb42c6d94
ckinyua004 <njauc85@gmail.com>
2025-06-20 23:35:44 +0300

    Added scoreboard, snake collisions with food and walls, snake ability to grow

--- 21.SnakeGame/main.py
+++ 21.SnakeGame/main.py
@@ -1,15 +1,21 @@
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
+turtle = Turtle()
 screen.setup(width=600, height=600)
 screen.bgcolor("green")
 screen.title("MY SNAKE GAME")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
+score = 0
 game_is_on = True
 while game_is_on:
     screen.update()
@@ -22,5 +28,19 @@ while game_is_on:
     screen.onkey(snake.left, "Left")
     screen.onkey(snake.right, "Right")
     
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
--- 21.SnakeGame/snake.py
+++ 21.SnakeGame/snake.py
@@ -14,12 +14,19 @@ class Snake:
         
     def createSnake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            #new_segment.shapesize(stretch_wid=1, stretch_len=1)
-            new_segment.color("blue")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        #new_segment.shapesize(stretch_wid=1, stretch_len=1)
+        new_segment.color("blue")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1 ,0 ,-1):
